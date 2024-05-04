5b74427d7238e001f7b8f4fa2d572edce55a5129
schoeserflo <schoeser.florian@gmail.com>
2024-05-04 18:01:06 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/Content.js 	new file:   src/components/Footer.js 	new file:   src/components/Header.js 	modified:   src/index.js

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+  return (
+    <main>
+      <h1>Willkommen auf meiner Website!</h1>
+      <p>Hier kommt der Hauptinhalt der Seite.</p>
+    </main>
+  );
+}
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Mein Website. Alle Rechte vorbehalten.</p>
+      <p><a href="/impressum">Impressum</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
