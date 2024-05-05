aa86072739fa17774ca4027cb735f7c995a5a51a
schoeserflo <schoeser.florian@gmail.com>
2024-05-05 20:04:17 +0200

    modified:   src/components/Tabelle.js

--- src/components/Tabelle.js
+++ src/components/Tabelle.js
@@ -36,6 +36,132 @@ function Co2Tabelle() {
           <td>1100</td>
           <td></td>
         </tr>
+        <tr>
+          <td>5</td>
+          <td>Indien</td>
+          <td>2200</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td>Brasilien</td>
+          <td>900</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>Russland</td>
+          <td>1600</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>8</td>
+          <td>Frankreich</td>
+          <td>350</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>9</td>
+          <td>Kanada</td>
+          <td>600</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>10</td>
+          <td>Südkorea</td>
+          <td>500</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>11</td>
+          <td>Global Oil Corp.</td>
+          <td>300</td>
+          <td>USA</td>
+        </tr>
+        <tr>
+          <td>12</td>
+          <td>Worldwide Mining Co.</td>
+          <td>250</td>
+          <td>Russland</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>AutoMacher AG</td>
+          <td>150</td>
+          <td>Deutschland</td>
+        </tr>
+        <tr>
+          <td>14</td>
+          <td>TechGLobal Ltd.</td>
+          <td>100</td>
+          <td>Südkorea</td>
+        </tr>
+        <tr>
+          <td>15</td>
+          <td>EcoEnergy Solutions</td>
+          <td>120</td>
+          <td>Brasilien</td>
+        </tr>
+        <tr>
+          <td>16</td>
+          <td>SteelPro Industries</td>
+          <td>400</td>
+          <td>China</td>
+        </tr>
+        <tr>
+          <td>17</td>
+          <td>EuroChemicals</td>
+          <td>200</td>
+          <td>Frankreich</td>
+        </tr>
+        <tr>
+          <td>18</td>
+          <td>AgriFood</td>
+          <td>80</td>
+          <td>Indien</td>
+        </tr>
+        <tr>
+          <td>19</td>
+          <td>Northern Timber</td>
+          <td>70</td>
+          <td>Kanada</td>
+        </tr>
+        <tr>
+          <td>20</td>
+          <td>Rapid Construction Group</td>
+          <td>220</td>
+          <td>Russland</td>
+        </tr>
+        <tr>
+          <td>21</td>
+          <td>LuxAutoDesigns</td>
+          <td>60</td>
+          <td>Japan</td>
+        </tr>
+        <tr>
+          <td>22</td>
+          <td>SolarTech Innovations</td>
+          <td>20</td>
+          <td>USA</td>
+        </tr>
+        <tr>
+          <td>23</td>
+          <td>NextGen Electronics</td>
+          <td>90</td>
+          <td>China</td>
+        </tr>
+        <tr>
+          <td>24</td>
+          <td>SteelPro Industries</td>
+          <td>400</td>
+          <td>Kanada</td>
+        </tr>
+        <tr>
+          <td>25</td>
+          <td>Global Freight Systems</td>
+          <td>150</td>
+          <td>Indien</td>
+        </tr>
       </tbody>
     </Table>
   );
