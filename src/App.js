34ef855f12850a9a8df2b19efcbff87135f3bbeb
schoeserflo <schoeser.florian@gmail.com>
2024-05-10 15:11:38 +0200

    new file:   .vscode/settings.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/Homepage/Header.js 	new file:   src/components/Homepage/NoPage.js 	renamed:    src/components/Kohlenstoffdioxid/App_Co.js -> src/components/Kohlenstoffdioxid/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Homepage/Header';
 import Footer from './components//Homepage/Footer';
 import Content from './components//Homepage/Content';
 
-
 function App() {
   return (
       <div>
--- src/components/Homepage/Header.js
+++ src/components/Homepage/Header.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Container, Row, Col, Navbar,} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import CO from '../Kohlenstoffdioxid/App_Co';
+import { Container, Row, Col, Navbar, Nav} from 'react-bootstrap';
 
-function Header() {
-  return (
+
+    function Header() {
+      return (
     <Navbar expand="lg" style={{ backgroundColor: '#4CAF50' }}>
-      <Container fluid>
-        <Row className="w-100">
-          <Col xs={4} className="d-flex align-items-center justify-content-start">
-            <Navbar.Brand href='#' style={{ color: 'white' }}>Green-Hand</Navbar.Brand>
-          </Col>
-          <Col xs={4} className="d-flex align-items-center justify-content-center">
-            <img src="/Green.png" alt="Firmenlogo" style={{ height: '50px' }} />
-          </Col>
-          <Col xs={4} className="d-flex align-items-center justify-content-end">
-          <Navbar.Brand href= <Link to ={CO}>Kohlenstoffdioxid </Link> style={{ color: 'white' }}>Kohlenstoffdioxid</Navbar.Brand>
-          </Col>
-        </Row>
-      </Container>
-    </Navbar>
+    <Container fluid>
+      <Row className="w-100">
+        <Col xs={4} className="d-flex align-items-center justify-content-start">
+          <Navbar.Brand href='#' style={{ color: 'white' }}>Green-Hand</Navbar.Brand>
+        </Col>
+        <Col xs={4} className="d-flex align-items-center justify-content-center">
+          <img src="/Green.png" alt="Firmenlogo" style={{ height: '50px' }} />
+        </Col>
+        <Col xs={4} className="d-flex align-items-center justify-content-end">
+        <Nav.Link href='Co' style={{ color: 'white' }}>Über CO2</Nav.Link>
+        </Col>
+
+      </Row>
+    </Container>
+  </Navbar>
   );
 }
 export default Header
